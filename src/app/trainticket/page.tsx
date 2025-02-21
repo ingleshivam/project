6f6840621b9f41364bea3ed83ec593ec9144c5b0
Shivam Ingle <75012378+ingleshivam@users.noreply.github.com>
2025-02-21 12:00:33 +0530

    Update page.tsx

--- src/app/trainticket/page.tsx
+++ src/app/trainticket/page.tsx
@@ -32,12 +32,12 @@ export default function TrainTicket() {
             <div className="space-y-4">
               <div className="grid grid-cols-2 gap-4">
                 <div>
-                  <p className="text-sm opacity-80">S</p>
+                  <div className="p-4 rounded-full"><p className="text-sm opacity-80">S</p></div>
                   <p className="text-sm">D</p>
                 </div>
                 <div>
                   <p className="text-sm opacity-80">Manmad मनमाड</p>
-                  <p className="font-bold">DAKSHINESWAR</p>
+                  
                   <p className="text-sm">Parbhani परभणी</p>
                 </div>
               </div>
