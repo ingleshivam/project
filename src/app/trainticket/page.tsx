efa901b2767922e4780eebd5c08e0b7e8c0eb4cd
Shivam Ingle <75012378+ingleshivam@users.noreply.github.com>
2025-04-29 22:08:13 +0530

    Update page.tsx

--- src/app/trainticket/page.tsx
+++ src/app/trainticket/page.tsx
@@ -32,17 +32,15 @@ export default function TrainTicket() {
             <div className="space-y-4">
               <div className="grid grid-cols-6 gap-4">
                 <div className="grid grid-rows-2 gap-4">
-                                <div className="flex items-center gap-4">
-                  <div style={{backgroundColor:"#F40076"}} className="rounded-full ml-2 w-6 h-6 flex item-center justify-center"><p className="text-sm opacity-80">S</p></div>
-                  <p className="text-sm opacity-80">Manmad मनमाड</p>
-                    </div>              
-                </div>
+                  <div className="flex items-center gap-4">
+                       <div style={{backgroundColor:"#F40076"}} className="rounded-full ml-2 w-6 h-6 flex item-center justify-center"><p className="text-sm opacity-80">S</p></div>
+                       <p className="text-sm opacity-80">Manmad मनमाड</p>
+                  </div>              
                 <div className="flex items-center gap-4">
-                  <div style={{backgroundColor:"#F40076"}} className="rounded-full ml-2 w-6 h-6 flex item-center justify-center"><p className="text-sm opacity-80">D</p></div>
-                
+                  <div style={{backgroundColor:"#F40076"}} className="rounded-full ml-2 w-6 h-6 flex item-center justify-center"><p className="text-sm opacity-80">D</p></div>         
                   <p className="text-sm">Parbhani परभणी</p>
                 </div>
-            
+            </div>
               </div>
 
               <div className="grid grid-cols-2 gap-4">
